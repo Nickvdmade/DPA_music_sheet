8d9b2a009a6076a048d1858d7c18c12c730691ba
Nickvdmade <nickvdmade@gmail.com>
2017-09-17 13:46:54 +0200

    Aanpassigen week 1 & 2

---
+++ DPA_Musicsheets/MusicProperties/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.MusicProperties
+{
+    class Note
+    {
+        private char pitch;
+        private int octave;
+        private int change; //1 voor kruis, -1 voor mol
+        private double length;
+
+        public Note(char notePitch, double noteLength, int noteChange = 0, int noteOctave = 0)
+        {
+            pitch = notePitch;
+            octave = noteOctave;
+            change = noteChange;
+            length = noteLength;
+        }
+    }
+}
---
+++ DPA_Musicsheets/MusicProperties/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DPA_Musicsheets.MusicProperties
+{
+    class Staff
+    {
+        private List<Bar> bars = new List<Bar>();
+        private int tempo;
+
+
+    }
+}
